b5d27e2e9774bcaefe47725b08d8b5ed2077988b
Marc Newton <php@marcnewton.info>
2020-07-15 01:13:49 +0100

    Create vuetify.js

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Vuetify, { 
+//    VBtn,
+//    VIcon,
+//    VLayout
+//    VSnackbar
+} from 'vuetify/lib'
+
+Vue.use(Vuetify, {
+  components: { 
+//    VBtn, 
+//    VIcon, 
+//    VLayout, 
+//    VSnackbar }
+})
+
+export default new Vuetify({
+//  rtl: false,
+//  theme: {
+//    options: {
+//      customProperties: true,
+//      cspNonce: 'dQuWEr2sEfG'
+//    },
+//    themes: {
+//      light: {
+//        primary: '#6ec5a0',
+//        secondary: '#424242',
+//        accent: '#3bafbd',
+//        error: '#FF5252',
+//        info: '#2196F3',
+//        success: '#6ec5a0',
+//        warning: '#FFC107'
+//      }
+//    }
+//  }
+})
